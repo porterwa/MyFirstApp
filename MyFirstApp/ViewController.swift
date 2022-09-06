e5b3a0c6427357a4cad9263e152d068b65a50a52
porterwa <porterwa@appstate.edu>
2022-09-06 18:07:08 -0400

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Mac User on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
