1d6c373cd9acc3ebb70e920037166de48c328ad4
porterwa <porterwa@appstate.edu>
2022-09-10 22:47:10 -0400

    chapter 3. Added text field and connected it to button and label.

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet weak var textMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
 
-
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textMessage.text?.uppercased()
+    }
+    
 }
 
